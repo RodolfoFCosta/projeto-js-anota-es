8726ee86962eb1e91a8f0fbcdbcfb6f9e349af95
Rodolfo Fialho da Costa <rodolfofialho905@gmail.com>
2022-11-23 21:29:26 -0300

    atualizando script

--- js/script.js
+++ js/script.js
@@ -1,32 +1,74 @@
-//contador inicial
-let count = 0;
+// Selecionar elementos
+const notesContainer = document.querySelector("#notes-container")
 
-//seleciona o valor do button
-const value = document.getElementById('value');
-const btns = document.querySelectorAll(".btn")
+const noteInput = document.querySelector("#note-container")
 
-btns.forEach(function (btn) {
-    btn.addEventListener('click', (e) => {
-        const estilo = e.currentTarget.classList
-        if (estilo.contains('subtrai')) {
-            count--
-        }
-        else if (estilo.contains('soma')) {
-            count++
-        } else {
-            count = 0
-        }
-
-        if (count < 0) {
-            value.style.color = "#ea4335"
-        }
-        else if (count > 0) {
-            value.style.color = "#34A853"
-        } else {
-            value.style.color = "#ffff"
-        }
+const addNoteBtn = document.querySelector(".add-note")
 
 
-        value.textContent = count;
+// Funcoes
+function showNotes() {
+    getNotes().forEach((note) => {
+        const noteElement = createNote(note.id, note.content, note.fixed);
+
+        notesContainer.appendChild(noteElement)
     })
-})
+}
+
+function addNote() {
+    const notes = getNotes();
+
+    const noteObject = {
+        id: generateId(),
+        content: noteInput.value,
+        fixed: false,
+    };
+
+    const noteElement = createNote(noteObject.id, noteObject.content);
+
+    notesContainer.appendChild(noteElement)
+
+    saveNotes(notes)
+
+    noteInput.value = "";
+}
+
+function generateId() {
+    return Math.floor(Math.random() * 5000);
+}
+
+function createNote(id, content, fixed) {
+
+    const element = document.createElement("div")
+
+    element.classList.add("note")
+
+    const textarea = document.createElement("textarea")
+
+    textarea.value = content
+
+    textarea.placeholder = "Adicione algum texto..."
+
+    element.appendChild(textarea)
+
+    return element;
+}
+
+// Local Storage
+function getNotes() {
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+
+    return notes;
+}
+
+
+function saveNotes(notes) {
+    localStorage.setItem("notes", JSON.stringify(notes))
+}
+
+
+// Eventos
+addNoteBtn.addEventListener("click", () => addNote())
+
+// inicialização
+showNotes();
